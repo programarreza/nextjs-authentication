f988d5870964c779e3b820c17eaaf0f535ef9b4f
programarreza <programarreza@gmail.com>
2024-09-05 21:24:53 +0600

    google auth done

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -78,7 +78,14 @@ const LoginPage = () => {
           </form>
           <p className="text-center">Or Sign Up Using</p>
           <div className="flex justify-center mb-10 mt-2">
-            <button className="btn btn-circle ">
+            <button
+              className="btn btn-circle"
+              onClick={() =>
+                signIn("google", {
+                  callbackUrl: "http://localhost:3000/dashboard",
+                })
+              }
+            >
               <Image
                 src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"
                 width={50}
@@ -86,9 +93,14 @@ const LoginPage = () => {
                 alt="google logo"
               />
             </button>
-            <button className="btn btn-circle" onClick={() => signIn("github", {
-              callbackUrl: "http://localhost:3000/dashboard"
-            })}>
+            <button
+              className="btn btn-circle"
+              onClick={() =>
+                signIn("github", {
+                  callbackUrl: "http://localhost:3000/dashboard",
+                })
+              }
+            >
               <Image
                 src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
                 width={35}
--- src/utils/authOptions.ts
+++ src/utils/authOptions.ts
@@ -1,4 +1,5 @@
 import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 import { NextAuthOptions } from "next-auth";
 
 export const authOptions: NextAuthOptions = {
@@ -8,6 +9,10 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
   ],
-  secret: process.env.NEXTAUTH_SECRET
+  secret: process.env.NEXTAUTH_SECRET,
 };
